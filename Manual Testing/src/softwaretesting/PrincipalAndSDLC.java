116c4e038ed643d2c4b6189785a23c00456fa7dc
Dell <Dell@DESKTOP-E1888L2>
2022-03-16 20:14:16 +0530

    manual testing

---
+++ Manual Testing/src/softwaretesting/PrincipalAndSDLC.java
@@ -0,0 +1,59 @@
+package softwaretesting;
+
+public class PrincipalAndSDLC {
+	// Software is a collection of specialialized programs which takes user input and generate desire output
+	
+	//Testing is process of checking the given software or applicaton is generating desire output.
+	
+	//ST Involvement---client --BA ---Developer--Tester--
+	
+	//Software testing principal
+ 	// 7 th principal
+	/* Testing shows the presence of defect
+	 * Exhaustive testing impossible
+	 * Early testing
+	 * Defect clustering
+	 * Pesticide Paradox
+	 * Testing is context dependent
+	 * absence of error fallacy
+	 */
+	
+	
+	// SDLC MODEL
+	
+	// Requairement --Analysis--Design--Coding--Testing --Deployment/Maintenance.
+	
+	// 5 Models
+	/* Waterfall model
+	 * Spiral model
+	 * V model
+	 * Prototype model
+	 * Agile 
+	 */
+	
+	//Types of Testing
+	/* White Box Testing -----------Developer
+	 *  Black Box Testing-----------Tester
+	 *   Gray Box Testing-----------Developer/Tester-if coding knowedge 
+	 */
+	
+	// Level of Testing
+	/* Unit Testing
+	 * Integration Testing
+	 * System Testing
+	 * UAT (User Acceptance Testing)
+	 */
+	
+	//Integration Testing                 // System Testing
+	/* Top-Down                            Functional Testing          Non Functional Testing
+	 * Bottom-Up                           Smoke Testing                Usability, Load,Performance, Recovery,
+	 * Sandwich                            Sanity                       Compatibility, Configration, Installation.
+	 * Big Bank                            Integration
+	 */                                //    System
+ //  Regression
+	                                      //  UAT
+	
+	
+	
+
+}
